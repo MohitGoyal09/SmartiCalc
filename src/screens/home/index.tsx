83467e9bed85fc046b52c7486950bc9ce876862d
MohitGoyal09 <goel0632@gmail.com>
2024-09-22 01:07:19 +0530

    Update

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,59 +1,140 @@
-import { useState , useRef } from "react"
+import { useState, useRef, useEffect } from "react"
+import { Button } from '@/components/ui/button';
+import {SWATCHES} from '@/constants';
+import { ColorSwatch, Group } from '@mantine/core';
+import axios from 'axios';
 
+interface Response {
+  expr : String;
+  result : String;
+  assign : boolean;
+
+}
+
+interface GeneratedResult {
+  expression : String;
+  answer : String;
+
+}
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [isDrawing , setisDrawing] = useState(false);
-  useEffect( () => {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [color, setColor] = useState('rgb(255, 255, 255)');
+  const [reset , setReset] = useState(false);
+  const[ result , setResult] = useState<GeneratedResult>();
+  const [dictOfVars , setDictOfVars] = useState({});
+
+  useEffect(() => {
+    if (reset){
+       resetCanvas();
+       setReset(false);
+    }
+  },[reset])
+  useEffect(() => {
     const canvas = canvasRef.current;
-    if (canvas){
-        const ctx = canvas.getContext('2d');
-        if (ctx){
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
         canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        canvas.height = window.innerHeight - canvas.offsetTop;
         ctx.lineCap = 'round';
         ctx.lineWidth = 3;
-        
-        }
+      }
     }
-  })
-  const StartDrawing = ( e : React.MouseEvent<HTMLCanvasElement>) => {
+  }, [])
+
+  const sendData = async () => {
     const canvas = canvasRef.current;
-    if (canvas){
-        canvas.style.background = 'black';
-        const ctx = canvas.getContext('2d');
-        if (ctx){
-            ctx.beginPath();
-            ctx.moveTo(e.nativeEvent.offsetX , e.nativeEvent.offsetY);
-            setisDrawing(true);
+    if ( canvas ){
+      const reponse = await axios({
+        method: 'post',
+        url : `${import.meta.env.VITE_API_URL}/calculate`,
+        data : {
+          image : canvas.toDataURL('image/png'),
+          dict_of_vars : dictOfVars,
         }
+      });
+      const resp = await reponse.data;
+      console.log('reponse :' , resp);
     }
-    
   }
-  const stopDrawing = () => {
-        setisDrawing(false);
+
+   const resetCanvas = () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
     }
 
-   const draw = (e : React.MouseEvent<HTMLCanvasElement>) => {
-     if (!isDrawing) return;
-     const canvas = canvasRef.current;
-     if (canvas){
-        const ctx = canvas.getContext('2d');
-        if (ctx){
-            ctx.strokeStyle = 'white';
-            ctx.lineTo(e.nativeEvent.offsetX , e.nativeEvent.offsetY);
-            ctx.stroke();
-        }
+  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.style.background = 'black';
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.beginPath();
+        ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        setIsDrawing(true);
+      }
+    }
+  }
 
-   }
-  return(
-    <canvas ref={canvasRef} 
-    id ='canvas'
-    className="aboslute top-0 left-0 w-full h-full"
-    onMouseDown={StartDrawing}
-    onMouseUp={stopDrawing}
-    onMouseOut={stopDrawing}
+  const stopDrawing = () => {
+    setIsDrawing(false);
+  }
+
+  const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing) return;
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.strokeStyle = color;
+        ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        ctx.stroke();
+      }
+    }
+  }
+
+  return (
+    <>
+    <div className="grid grid-cols-3 gap-2">
+      <Button
+                    onClick={() => setReset(true)}
+                    className='z-20 bg-black text-white'
+                    variant='default' 
+                    color='black'
+                >
+                    Reset
+                </Button>
+                <Group className='z-20'>
+                    {SWATCHES.map((swatch : string) => (
+                        <ColorSwatch key={swatch} color={swatch} onClick={() => setColor(swatch)} />
+                    ))}
+                </Group>
+                <Button
+                    onClick={sendData}
+                    className='z-20 bg-black text-white'
+                    variant='default'
+                    color='white'
+                >
+                    Run
+                </Button>
+
+    </div>
+    <canvas 
+      ref={canvasRef} 
+      id="canvas"
+      className="absolute top-0 left-0 w-full h-full"
+      onMouseDown={startDrawing}
+      onMouseUp={stopDrawing}
+      onMouseOut={stopDrawing}
+      onMouseMove={draw}
     ></canvas>
+    </>
   )
 }
-
-}
+ 
